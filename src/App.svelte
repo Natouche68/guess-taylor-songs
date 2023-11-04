7baa008d75eea2431a191b729d8bfea02508a128
Natouche68 <ngrossheny@gmail.com>
2023-11-04 18:35:04 +0100

    Add a title and styles

--- src/App.svelte
+++ src/App.svelte
@@ -1,1 +1,12 @@
-Guess Taylor's Songs
+<div class="title">Guess Taylor's Songs</div>
+
+<style>
+	.title {
+		padding: 0.6rem;
+		text-align: center;
+		font-family: "Playball", cursive;
+		font-size: 3.6rem;
+		font-weight: 700;
+		border-bottom: 3px solid var(--text-color);
+	}
+</style>
