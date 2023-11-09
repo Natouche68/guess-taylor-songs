b2d65be3e47a568b356c4dd62c10ada9ae2876b0
Natouche68 <ngrossheny@gmail.com>
2023-11-09 19:07:46 +0100

    Add the song list

---
+++ src/lib/songs.js
@@ -0,0 +1,90 @@
+export const songs = [
+	{
+		name: "Shake It Off",
+		id: "kx0rALx7PIg",
+	},
+	{
+		name: "Blank Space",
+		id: "pZN6ZCAeSE4",
+	},
+	{
+		name: "Wildest Dreams",
+		id: "i_l8dqt3WSk",
+	},
+	{
+		name: "Bad Blood",
+		id: "th1JDYYPsHs",
+	},
+	{
+		name: "Welcome To New York",
+		id: "mjPdW-Ty6ww",
+	},
+	{
+		name: "We Are Never Ever Getting Back Together",
+		id: "fOH6Vtvy16g",
+	},
+	{
+		name: "I Knew You Were Trouble",
+		id: "nDgme9eQ_Sg",
+	},
+	{
+		name: "22",
+		id: "_9lhoJE5IWM",
+	},
+	{
+		name: "All Too Well",
+		id: "FezDzTKfH3s",
+	},
+	{
+		name: "Cruel Summer",
+		id: "niZ6U-NRmCk",
+	},
+	{
+		name: "Lover",
+		id: "CYb3MQyt0p0",
+	},
+	{
+		name: "The Man",
+		id: "UCQkNP-Qo8o",
+	},
+	{
+		name: "Paper Rings",
+		id: "qe3EdhXP7qE",
+	},
+	{
+		name: "Anti-Hero",
+		id: "Xbp-OORsRE8",
+	},
+	{
+		name: "Karma",
+		id: "ac5cdxnhv9I",
+	},
+	{
+		name: "Bejeweled",
+		id: "torJ0cvpHoQ",
+	},
+	{
+		name: "Love Story",
+		id: "AXbuaWJ69UA",
+	},
+	{
+		name: "You Belong With Me",
+		id: "_U7UPPXINqI",
+	},
+	{
+		name: "Enchanted",
+		id: "CN3d35Xt7FU",
+	},
+	{
+		name: "Look What You Made Me Do",
+		id: "5Rii88RVzsU",
+	},
+	{
+		name: "Delicate",
+		id: "f41v_7d0KCI",
+	},
+	{
+		name: "Don't Blame Me",
+		id: "6u3E2Z7iXtU",
+	},
+];
